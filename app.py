6ceaf7120c57d28891cbf697216a32a5c48c7b34
Thea Barnes <thea@tsbarnes.com>
2021-06-09 05:16:13 -0400

    Testing

--- app.py
+++ app.py
@@ -7,23 +7,25 @@ from multiprocessing import Process
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     write_string('Hello World!', offset_x=0)
     return 'Hello World!'
 
-def scroll_loop():
+
+def apploop():
+    app.run()
+
+
+if __name__ == '__main__':
+    p = Process(target=apploop)
+    p.start()
+
+    write_string('Waiting...', offset_x=0)
     while True:
         scroll()
         show()
         time.sleep(1)
-
-if __name__ == '__main__':
-    write_string('Waiting...', offset_x=0)
-
-    p = Process(target=scroll_loop)
-    p.start()
-
-    app.run()
 
     p.join()
